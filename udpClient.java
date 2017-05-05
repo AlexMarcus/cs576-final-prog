4ab32b0414f2fc3c5682e2c55efc0325d17adf3e
johnNull <nullj27@gmail.com>
2017-05-05 14:05:12 -0400

    one message java udp socket

--- udpClient.java
+++ udpClient.java
@@ -3,8 +3,7 @@ import java.net.*;
 
 public class udpClient{
 	public static void main(String[] args) throws IOException{
-		byte[] sendBuf = new byte[256];
-		sendBuf = "Successful ping".getBytes();
+		byte[] sendBuf = "Successful ping".getBytes();
 		DatagramSocket socket = new DatagramSocket();
 		InetAddress address = InetAddress.getByName("149.125.39.106");
 		DatagramPacket packet = new DatagramPacket(sendBuf, sendBuf.length, address, 12000);
